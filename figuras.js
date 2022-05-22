c226e5312515c3c130c576e341db09fb1a9f4fd6
Roweshil <x-vdm@hotmail.com>
2022-05-22 12:37:05 -0500

    cambios al js

---
+++ figuras.js
@@ -0,0 +1,24 @@
+
+
+function cuadrado(){
+const ladoCuadrado = 5;
+const perimetroCuadrado = ladoCuadrado * 4;
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+
+
+}
+
+function triagulo(){
+
+const ladoTriangulo1 = 6,ladoTriangulo2 = 6, baseTriangulo = 4, alturaTriangulo=5.5;
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
+const areaTriangulo = (baseTriangulo * alturaTriangulo) /2;
+}
+
+function circulo(){ 
+const radioCirculo = 4, diametroCirculo= radioCirculo * 2;
+const PI = Math.PI;
+const perimetroCirculo = diametroCirculo * PI;
+const areaCirculo= (radioCirculo* radioCirculo) * PI;
+
+}
